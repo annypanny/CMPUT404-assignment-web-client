46e7b2d81068e8d7e9bba81725b5dc778d2d77af
annypanny <anny_pan@live.cn>
2016-09-29 20:37:40 -0600

    >_<

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,16 +37,29 @@ class HTTPClient(object):
 
     def connect(self, host, port):
         # use sockets!
-        return None
+        if port == None:
+            port = 80
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            clientSocket.connect((host,port))
+            print("connection succeeded")
+        except Exception as e:
+            print("socket error")
+            sys.exit()
+        return clientSocket
 
     def get_code(self, data):
-        return None
+        code = int(data.split(' ')[1])
+        return code
 
     def get_headers(self,data):
-        return None
+        #headers = data.split('\r\n\r\n')[0]
+        headers = data.split('\r\n\r\n')[1:-2]
+        return headers
 
     def get_body(self, data):
-        return None
+        body = data.split('\r\n\r\n')[1]
+        return body
 
     # read everything from the socket
     def recvall(self, sock):
@@ -60,14 +73,69 @@ class HTTPClient(object):
                 done = not part
         return str(buffer)
 
+    def get_url_info(self,url):
+        if url[:7] == "http://":
+            url = url[7:]
+
+        if "/" in url:
+            pos = url.index("/")
+            host = url[:pos]
+            path = url[pos:]
+
+        if ":" in url:
+            pos = url.index(":")
+            host = url[:pos]
+            port = url[pos+1:]
+
+        return host, path, port
+
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        host, path, port = self.get_url_info(url)
+
+        request = "GET " + path + " HTTP/1.1\r\n"\
+                  "Host: " + host + "\r\n"\
+                  "Accept: */*\r\n"\
+                  "connection: close\r\n\r\n"
+
+        clientSocket = self.connect(host, port)
+        clientSocket.sendall(request)
+
+        response = self.recvall(clientSocket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+
+        host, path, port = self.get_url_info(url)
+
+        if args != None:
+            content = urllib.urlencode(args)
+        else:
+            content = ""
+
+        content_length = str(len(content))
+
+        request = "POSE " + path + " HTTP/1.1\r\n"\
+                  "Host: " + host + "\r\n"\
+                  "Accept: */*\r\n"\
+                  "Content-Length: " + content_length + "\r\n"\
+                  "connection: close\r\n\r\n" + content + "\r\n\r\n"
+
+        clientSocket = self.connect(host, port)
+        clientSocket.sendall(request)
+
+        response = self.recvall(clientSocket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -75,7 +143,7 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
-    
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -85,4 +153,4 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 3):
         print client.command( sys.argv[2], sys.argv[1] )
     else:
-        print client.command( sys.argv[1] )   
+        print client.command( sys.argv[1] )
